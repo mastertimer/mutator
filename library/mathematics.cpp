4131a8d1d5ab87b7f39c29b048157d44b2c2c873
popovig <popovig@avpt.ru>
2020-11-03 11:36:28 +0300

    ввел модуль математики

---
+++ library/mathematics.cpp
@@ -0,0 +1,149 @@
+#include "mathematics.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void _basic_statistics::push(i64 x)
+{
+	if (x - start >= (i64)data.size()) data.resize(x - start + 1, 0);
+	if (x < start) { data.insert(data.begin(), start - x, 0); start = x; }
+	data[x - start]++;
+}
+
+i64 _basic_statistics::number() const noexcept
+{
+	i64 s = 0;
+	for (auto i : data) s += i;
+	return s;
+}
+
+i64 _basic_statistics::number(i64 be, i64 en) const noexcept
+{
+	be -= start;
+	en -= start;
+	if (be < 0) be = 0;
+	if (en > (i64)data.size()) en = (i64)data.size();
+	i64 s = 0;
+	for (i64 i = be; i < en; i++) s += data[i];
+	return s;
+}
+
+i64 _basic_statistics::operator[](i64 x) const noexcept
+{
+	if (x - start >= (i64)data.size()) return 0;
+	if (x < start) return 0;
+	return data[x - start];
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+i64 _statistics::number(i64 be, i64 en) const noexcept
+{
+	auto li1 = std::lower_bound(data.begin(), data.end(), be, [](_one_stat a, i64 b) { return (a.value < b); });
+	auto li2 = std::lower_bound(data.begin(), data.end(), en, [](_one_stat a, i64 b) { return (a.value < b); });
+	return number(li1, li2);
+}
+
+i64 _statistics::number(_cit be, _cit en) const noexcept
+{
+	i64 s = 0;
+	for (auto i = be; i != en; ++i) s += i->number;
+	return s;
+}
+
+bool _statistics::operator==(const _statistics& a) const noexcept
+{
+	if (data.size() != a.data.size()) return false;
+	for (i64 i = 0; i < (i64)data.size(); i++)
+		if (data[i] != a.data[i]) return false;
+	return true;
+}
+
+void _statistics::operator+=(const _statistics& a)
+{
+	_statistics res;
+	auto i = data.begin();
+	auto ai = a.data.begin();
+	while ((i != data.end()) && (ai != a.data.end()))
+	{
+		if (i->value < ai->value)
+		{
+			res.data.push_back(*i);
+			++i;
+			continue;
+		}
+		if (ai->value < i->value)
+		{
+			res.data.push_back(*ai);
+			++ai;
+			continue;
+		}
+		res.data.push_back({ i->value, i->number + ai->number });
+		++i;
+		++ai;
+	}
+	for (; i != data.end(); ++i) res.data.push_back(*i);
+	for (; ai != a.data.end(); ++ai) res.data.push_back(*ai);
+	*this = res;
+}
+
+void _statistics::operator=(const _basic_statistics& a)
+{
+	i64 k = 0;
+	for (auto i : a.data) if (i) k++;
+	data.clear();
+	data.reserve(k);
+	for (i64 i = 0; i < (i64)a.data.size(); i++)
+		if (a.data[i]) data.push_back({ a.start + i, a.data[i] });
+}
+
+i64 _statistics::first_zero()
+{
+	for (i64 i = 1; i < (i64)data.size(); i++)
+		if (data[i].value != data[i - 1].value + 1)
+			return data[i - 1].value + 1;
+	return -1;
+}
+
+double _statistics::arithmetic_size(_it be, _it en)
+{
+	double s = 0;
+	double n = 1.0 / number(be, en);
+	for (auto i = be; i != en; ++i) s += i->number * log(i->number * n);
+	return -s / log(2.0);
+}
+
+_matrix _statistics::to_matrix()
+{
+	_matrix res(data.size(), 2);
+	for (i64 i = 0; i < (i64)data.size(); i++)
+	{
+		res[i][0] = data[i].value;
+		res[i][1] = data[i].number;
+	}
+	return res;
+}
+
+_matrix _statistics::to_matrix(i64 mi, i64 ma)
+{
+	i64 n;
+	i64 k = data.size() - 1;
+	for (n = 0; n <= k; n++) if (data[n].value >= mi) break;
+	for (; k >= 0; k--) if (data[k].value <= ma) break;
+	if (k < n) return {};
+	_matrix res(k - n + 1, 2);
+	for (i64 i = n; i <= k; i++)
+	{
+		res[i - n][0] = data[i].value;
+		res[i - n][1] = data[i].number;
+	}
+	return res;
+}
+
+i64 _statistics::operator[](i64 n) const noexcept
+{
+	auto li = std::lower_bound(data.begin(), data.end(), n, [](_one_stat a, i64 b) { return (a.value < b); });
+	if (li == data.end()) return 0;
+	return (li->value == n) ? li->number : 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- sable/sable_stat.h
+++ sable/sable_stat.h
@@ -1,20 +1,10 @@
 #pragma once
 
-#include "basic.h"
+#include "mathematics.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 constexpr i64 roffer = 20; // предложений продажи, предложений покупки ( ВСЕГО = roffer * 2 );
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-struct _one_stat // единица статистики
-{
-	i64 value;   // значение / цена
-	i64 number;  // количество
-
-	bool operator!=(_one_stat a) const noexcept { return ((value != a.value) || (number != a.number)); }
-};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -97,52 +87,6 @@ private:
 	bool read0(_prices& c);
 	bool read1(_prices& c);
 	bool read12(_one_stat* v1, std::vector<_one_stat>& v0);
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-struct _basic_statistics // базовая разреженная статистика с нулями
-{
-	std::vector<i64> data;
-	i64 start = 0;
-
-	void push(i64 x); // добавить число в статистику
-	i64 number() const noexcept;
-	i64 number(i64 be, i64 en) const noexcept; // количество в интервале [be,en)
-	i64 operator[](i64 x) const noexcept;
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-struct _statistics // сжатая статистика
-{
-	std::vector<_one_stat> data;
-	typedef std::vector<_one_stat>::iterator _it;
-	typedef std::vector<_one_stat>::const_iterator _cit;
-
-	_statistics() = default;
-	_statistics(const _basic_statistics& a) { *this = a; }
-
-	void clear() { data.clear(); }
-	i64 min_value() const noexcept { return (data.empty()) ? 0 : data.front().value; }
-	i64 max_value() const noexcept { return (data.empty()) ? 0 : data.back().value; }
-	i64 number(_cit be, _cit en) const noexcept; // количество в интервале [be,en)
-	i64 number(i64 be, i64 en) const noexcept; // количество в интервале [be,en)
-	i64 number() const noexcept { return number(data.begin(), data.end()); } // общее количество
-
-	i64 first_zero(); // номер первого нулевого элемента начиная со start (-1 если не нашлось)
-	i64 number_not_zero() { return data.size(); } // количество значений с ненулевым количеством
-	double arithmetic_size(_it be, _it en); // арифметический размер в битах
-	double arithmetic_size() { return arithmetic_size(data.begin(), data.end()); }
-
-	void operator=(const _basic_statistics& a);
-	void operator+=(const _statistics& a);
-	bool operator==(const _statistics& a) const noexcept;
-
-	i64 operator[](i64 n) const noexcept; // value -> number
-
-	_matrix to_matrix(); // два столбца - x и y
-	_matrix to_matrix(i64 mi, i64 ma); // два столбца - x и y
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
